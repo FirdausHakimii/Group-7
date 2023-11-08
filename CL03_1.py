4e59ee66a0eed1052109acdb4bb6a9851a417ad9
eyeman0 <aaiman37@graduate.com>
2023-11-08 14:36:41 +0800

    edit comment

--- CL03_1.py
+++ CL03_1.py
@@ -1,8 +1,8 @@
-a_int = int(input("Give a number: "))
-b_int ,c_int = 1,0 
-while b_int <= a_int :
-        c_int = c_int + b_int
-        b_int = b_int + 1 
-print (a_int,b_int,c_int) 
-print ("Result: ", float(c_int)/b_int - 1) 
+a_int = int(input("Give a number: "))                  #Input save into a_int
+b_int ,c_int = 1,0                                     #Initial b_int and c_int values
+while b_int <= a_int :                                 #The while loop - will compare b and a 
+        c_int = c_int + b_int                          #if true, increment c with b
+        b_int = b_int + 1                              #if true, increamnt b with 1
+print (a_int,b_int,c_int)                              #Display a,b and c values
+print ("Result: ", float(c_int)/b_int - 1)             #Diplay result
         #CL03-1.py
