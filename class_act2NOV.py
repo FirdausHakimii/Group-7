85fcf200fc317b4b11a81b63b0138f2eefef3449
Syarif <you@example.com>
2023-11-14 02:02:31 +0800

    idk

--- class_act2NOV.py
+++
@@ -1,7 +0,0 @@
-a_int = int(input("give a number: "))
-b_int,c_int = 1,0
-while b_int <= a_int :
-    c_int = c_int + b_int
-    b_int = b_int + 1
-print (a_int,b_int,c_int)
-print ("Result: ", float(c_int)/b_int - 1) 
